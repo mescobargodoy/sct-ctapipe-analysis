f3cd56fdee75447a8101f434d946882c1e27db58
mescobargodoy <mescob11@ucsc.edu>
2024-02-26 10:36:59 -0800

    Added extra option for removing underscores.

--- src/name_output_file.sh
+++ src/name_output_file.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 # Check if the correct number of arguments are provided
 if [ $# -lt 2 ]; then
-  echo "Usage: $0 <file path> <output file name ending> <string to remove from file (optional)>
+  echo "Usage: $0 <file path> <output file name ending> <string to remove from file (optional)> <no_underscore (optional)>
         Removes any path dependencies from name as well as .simtel.gz. It also adds ending of your choice.
-        
+        <no_underscore>: True/False. If true no underscore is added.
         Example: 
         ./name_output_h5.sh simtel/array.simtel.gz some_ending.h5 .simtel.gz
-        -> array_some_ending.h5" 
+        -> array_some_ending.h5
+        
+        or
+        ./name_output_h5.sh simtel/array.simtel.gz some_ending.h5 .simtel.gz True
+        -> arraysome_ending.h5" 
   exit 1
 fi
 
@@ -18,5 +22,8 @@ temp_name=$(basename "$file_path")
 base_name="${temp_name%$string_to_remove}"
 
 result="$base_name"_"$output_file_ending"
+if [ "$4" = "True" ]; then
+  result="$base_name$output_file_ending"
+fi
 
 echo $result
